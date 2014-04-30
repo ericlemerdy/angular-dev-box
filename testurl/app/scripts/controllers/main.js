59e825fe523a6fe403cfc8847269e8d7ce54d0c3
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-04-30 17:01:28 +0200

    Move data in controller.

--- testurl/app/scripts/controllers/main.js
+++ testurl/app/scripts/controllers/main.js
@@ -3,8 +3,15 @@
 angular.module('testurlApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+      {
+      	title:'HTML5 Boilerplate',
+      	description:'HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites.'
+      }, {
+        title:'AngularJS',
+      	description:'AngularJS is a toolset for building the framework most suited to your application development.'
+      }, {
+      	title:'Karma',
+      	description:'Spectacular Test Runner for JavaScript.'
+      }
     ];
   });
