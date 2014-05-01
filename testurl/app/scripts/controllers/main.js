48febbc432380812bfc7fc6c6d6e4fb7c8447bbc
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-05-01 07:03:39 +0200

    no nullable awesomething selector

--- testurl/app/scripts/controllers/main.js
+++ testurl/app/scripts/controllers/main.js
@@ -2,5 +2,7 @@
 
 angular.module('testurlApp')
   .controller('MainCtrl', function ($scope, awesomeThings) {
-    $scope.awesomeThings = awesomeThings.query();
+    $scope.awesomeThings = awesomeThings.query({}, function(data) {
+        $scope.selectedAwesomeThing = data[0];
+    });
   });
